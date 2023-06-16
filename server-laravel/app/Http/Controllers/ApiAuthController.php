cde385133368bcf1d5924a396c66fed3941a338c
Dmitry <dmitry.bogomolov@gmail.com>
2023-06-16 17:40:48 +0300

    initial commit for laravel part

---
+++ server-laravel/app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class ApiAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login details'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+        ]);
+    }
+
+}
