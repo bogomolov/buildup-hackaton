3f35bbdd18cad665ce9a1319d8ba824a19d227eb
Tokan Vlad <vlad.tokan96@mail.ru>
2023-06-18 03:04:02 +0300

    Доработка модели, скайбокс, лого

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -4,16 +4,22 @@ import {useDispatch} from 'react-redux';
 import {AppDispatch} from 'src/store/types';
 import {setFilter} from 'src/store/mapDrawData/mapDrawData.slice';
 
+import Button from 'src/components/button/Button';
+
+import logoOrange from './assets/images/logo-orange.png';
+
 import './Header.scss';
-import Button from "../button/Button";
 
 const Header = () => {
     const dispatch = useDispatch<AppDispatch>();
 
     return (
         <div className={'header'}>
-            <div>
-
+            <div className={'header__logo-container'}>
+                <img className={'header__logo'} src={logoOrange}/>
+                <p className={'header__logo-text'}>
+                    S.o.d.i
+                </p>
             </div>
             <div className={'header__button-container'}>
                 <Button handleClick={() => dispatch(setFilter('school'))} title={'школы'}/>
